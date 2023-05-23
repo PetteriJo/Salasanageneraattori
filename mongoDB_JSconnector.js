33394a252ff19e966e3cc90cdb6fd24d07e7d6cb
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2023-05-23 11:32:58 +0300

    Add files via upload
    Lisätty funktio joka etsii randomit sanat tietokannasta

--- mongoDB_JSconnector.js
+++ mongoDB_JSconnector.js
@@ -1,23 +1,49 @@
-const mongoose = require('mongoose');
-//this user only has reading permissions on the database, update and delete can't be called
-const atlasUri = "mongodb+srv://reader:eUC7Z1wWT9QA98mw@salasanageneraattori.apcumhq.mongodb.net/?retryWrites=true&w=majority"
+async function main() {
+	const mongoose = require('mongoose');
+	const MongoClient = require('mongodb').MongoClient;
+	//this user only has reading permissions on the database, update and delete can't be called
+	const atlasUri = "mongodb+srv://reader:eUC7Z1wWT9QA98mw@salasanageneraattori.apcumhq.mongodb.net/?retryWrites=true&w=majority"
+	const assert = require('assert');
 
-//attempt connection
-try {
-	mongoose.connect(atlasUri, 
-					{useNewUrlParser: true, useUnifiedTopology: true});
-	console.log("mongoose is connected", atlasUri)
+	const client = new MongoClient(atlasUri, 
+						{useNewUrlParser: true, useUnifiedTopology: true});
+
+
+	await client.connect((err) => {
+		const collection = client.db('salasanageneraattori').collection('sanasto');
+		console.log('Connected');
+		});
+	await findWord(client, 3);
+	console.log('FindWord excecuted');
+	client.close();
+	console.log('Connection closed.');
 }
-catch (e) {
-	console.log("unable to connect");
+
+
+main().catch(console.error);
+
+//hakee random sanoja resultsLimit numeron perusteella
+async function findWord(client, resultsLimit)
+{
+	const cursor = client
+		.db('salasanageneraattori')
+		.collection('sanasto')
+		.aggregate(
+			[{$sample: {size: resultsLimit}}]
+			)
+		
+
+	const results = await cursor.toArray();
+		if (results.length > 0) {
+		console.log(`Found ${results.length} listing(s):`);
+		results.forEach((result, i) => {
+			let sana;
+			sana = result.Hakusana;
+			console.log();
+			console.log(sana);
+		});
+
+	};
 }
+	
 
-//connection saved to variable
-const dbConnection = mongoose.connection;
-
-//log any possible errors
-dbConnection.on('error', (error) => {
-		console.warn('Warning', error);
-	}); 
-//wait for connection to load before doing anything else
-dbConnection.once('open', () => console.log('Good to go!'))
